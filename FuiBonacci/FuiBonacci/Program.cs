eb2236a9386471787348f191e8a7137f0eb754ca
Pioterus <piotr.lewando@gmail.com>
2019-09-21 15:50:11 +0200

    Kumite_2

---
+++ FuiBonacci/FuiBonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuiBonacci
+{
+    
+
+
+
+   
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                long a = 0;
+                long b = 1;
+                long n = 0;
+                Console.WriteLine("1 wyraz ciągu Fibonacciego = {0}", a);
+                Console.WriteLine("2 wyraz ciągu Fibonacciego = {0}", b);
+                for (int i = 2; i < 50; i++)
+                {
+                    n = a + b;
+                    a = b;
+                    b = n;
+                    Console.WriteLine("{0} wyraz ciągu Fibonacciego = {1}", i + 1, n);
+                }
+            }
+        }
+    }
+
