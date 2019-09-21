8ff07e75bda005c3bc21fc252a8742bb2c9fbb36
Pioterus <piotr.lewando@gmail.com>
2019-09-21 17:15:05 +0200

    kumite3

--- LoginIngest/LoginIngest/Program.cs
+++ LoginIngest/LoginIngest/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 
-namespace LoginIngest
+namespace WindowsFormsApplication1
 {
-    class Program
+    public partial class LoginForm : Form
     {
-        static void Main(string[] args)
+        public LoginForm()
         {
-            Console.WriteLine("Hello World!");
+            InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username;
+            string password;
+
+            username = TB_username.Text;
+            password = TB_password.Text;
+
+            if (User.Login(username, password))
+            {
+                Globals._Login = true;
+
+                // Close login form
+                this.Dispose(false);
+
+            }
+            else
+            {
+                MessageBox.Show("Login Failed");
+            }
+
+        }
+    }
+}
+Static void Main()
+{
+    Application.SetCompatibleTextRenderingDefault(false);
+    Application.EnableVisualStyles();
+    DialogResult result;
+    using (var loginForm = new LoginForm())
+        result = loginForm.ShowDialog();
+    if (result == DialogResult.OK)
+    {
+        // login was successful
+        Application.Run(new Mainform());
     }
 }
